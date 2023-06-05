a1f465a30d732a5f3ef93bca49f51c0f7cc64f7e
som3aware <ahmed.ismail@nodogoro.com>
2023-06-05 19:57:11 +0300

    refactor(App): add theme prop for paper & navigation providers

--- App.tsx
+++ App.tsx
@@ -3,13 +3,14 @@ import { PaperProvider } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 
 // internal deps
+import appTheme from './src/theme';
 import AppStackNavigator from './src/navigation/AppStackNavigator';
 
 // app entry point
 export default function App() {
   return (
-    <PaperProvider>
-      <NavigationContainer>
+    <PaperProvider theme={appTheme}>
+      <NavigationContainer theme={appTheme}>
         <AppStackNavigator />
       </NavigationContainer>
     </PaperProvider>
